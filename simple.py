1f3cdac8b61a73196f9b6f54728c1e595238217d
Yulin Zhu <zieft@hotmail.com>
2017-03-07 15:50:37 +0100

    add official solution to Exercise 1

--- simple.py
+++ simple.py
@@ -15,6 +15,26 @@ DATAFILE = "beatles-diskography.csv"
 
 def parse_file(datafile):
     data = []
+    with open(datafile, 'rb') as f:
+        header = f.readline().split(',')
+        counter = 0
+        for line in f:
+            if counter == 10:
+                break
+
+            fields = line.split(',')
+            entry = {}
+
+            for i, value in enumerate(fields):
+                entry[header[i].strip()] = value.strip()
+
+            data.append(entry)
+            counter += 1
+    return data
+
+
+def my_parse_file(datafile):
+    data = []
     raw_text = []
     with open(datafile, "r") as f:
         for line in f:
