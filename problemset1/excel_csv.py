4bf6734c123f6201d7ddad6c2b0d86a5a070357e
Yulin Zhu <zieft@hotmail.com>
2017-03-09 22:01:37 +0100

    finish function parse_file for excel_csv.py

--- problemset1/excel_csv.py
+++ problemset1/excel_csv.py
@@ -21,7 +21,7 @@ import os
 import csv
 from zipfile import ZipFile
 
-datafile = "2013_ERCOT_Hourly_Load_Data.xls"
+datafile = "C:\\Users\\zieft\\Dropbox\\PyCharmProjects_WIN\\MongoDB\\lesson_1\\2013_ERCOT_Hourly_Load_Data.xls"
 outfile = "2013_Max_Loads.csv"
 
 
@@ -33,7 +33,31 @@ def open_zip(datafile):
 def parse_file(datafile):
     workbook = xlrd.open_workbook(datafile)
     sheet = workbook.sheet_by_index(0)
-    data = None
+    data = [["Station", "Year", "Month", "Day", "Hour", "Max Load"]]
+    stations = sheet.row_values(0, start_colx=1, end_colx=-1)
+
+    station_values_dict = dict()
+    counter = 0
+    for station in stations:
+        if counter <= len(sheet.row_values(0, start_colx=1, end_colx=-1)):
+            station_values_dict[station] = sheet.col_values(counter, start_rowx=1, end_rowx=None)
+            counter += 1
+
+    counter2 = 1
+    for key in station_values_dict:
+        if counter2 < len(stations) + 1:
+            _ = []
+            _.append(stations[counter2 - 1])
+            station_max_load = max(station_values_dict[key])
+            max_load_index = station_values_dict[key].index(station_max_load)
+            max_time = xlrd.xldate_as_tuple(max_load_index, 0)
+            year, month, day, hour, _1, _2 = max_time
+            _.append(year)
+            _.append(month)
+            _.append(hour)
+            _.append(max(station_values_dict[key]))
+            data.append(_)
+        counter2 += 1
     # YOUR CODE HERE
     # Remember that you can use xlrd.xldate_as_tuple(sometime, 0) to convert
     # Excel date to Python tuple of (year, month, day, hour, minute, second)
@@ -41,6 +65,10 @@ def parse_file(datafile):
 
 
 def save_file(data, filename):
+    with open(outfile, 'w') as csvfile:
+        spamwriter = csv.writer(csvfile, delimiter=' ', quotechar='|', quoting=csv.QUOTE_MINIMAL)
+        spamwriter.writerow(['Spam'] * 5 + ['Baked Beans'])
+        spamwriter.writerow(['Spam', 'Lovely Spam', 'Wonderful Spam'])
 
 
 # YOUR CODE HERE
@@ -91,3 +119,10 @@ def test():
 
 if __name__ == "__main__":
     test()
+
+import csv
+
+with open('eggs.csv', 'w') as csvfile:
+    spamwriter = csv.writer(csvfile, delimiter=' ', quotechar='|', quoting=csv.QUOTE_MINIMAL)
+    spamwriter.writerow(['Spam'] * 5 + ['Baked Beans'])
+    spamwriter.writerow(['Spam', 'Lovely Spam', 'Wonderful Spam'])
