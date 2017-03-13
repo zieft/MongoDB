dbaaa6fbc85aab8efa525cf9348740dc328d2214
Yulin Zhu <zieft@hotmail.com>
2017-03-13 20:36:17 +0100

    add official answer

--- lesson_2/authors.py
+++ lesson_2/authors.py
@@ -13,13 +13,30 @@ def get_root(fname):
     return tree.getroot()
 
 
-def get_authors(root):
+def my_get_authors(root):
     authors = []
     for author in root.findall('./fm/bibl/aug/au'):
         data = {}
         data['fnm'] = author.find("fnm").text
         data['snm'] = author.find("snm").text
         data['email'] = author.find("email").text
+        authors.append(data)
+
+    return authors
+
+
+def get_author(root):
+    authors = []
+    for author in root.findall('./fm/bibl/aug/au'):
+        data = {
+            "fnm": None,
+            "snm": None,
+            "email": None
+        }
+        data["fnm"] = author.find('./fnm').text
+        data["snm"] = author.find('./snm').text
+        data["email"] = author.find('./email').text
+
         authors.append(data)
 
     return authors
