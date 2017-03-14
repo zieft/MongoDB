cd494a16b3b5a40a7b328994085686f18c59b255
Yulin Zhu <zieft@hotmail.com>
2017-03-14 14:23:16 +0100

    add official answer

--- lesson_2/authors.py
+++ lesson_2/authors.py
@@ -41,7 +41,9 @@ def get_authors(root):
         data["fnm"] = author.find('./fnm').text
         data["snm"] = author.find('./snm').text
         data["email"] = author.find('./email').text
-
+        insr = author.findall('./insr')
+        for i in insr:
+            data["insr"].append(i.attrib["iid"])
         authors.append(data)
 
     return authors
