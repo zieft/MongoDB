8e23c2ce96449a19da68a974377e0eabed893bbe
Yulin Zhu <zieft@hotmail.com>
2017-03-14 16:08:52 +0100

    fixed a small bug

--- lesson_2/html_extraction.py
+++ lesson_2/html_extraction.py
@@ -8,7 +8,7 @@ file = requests.get(URL)
 def options(soup, id):
     options_values = []
     carrier_list = soup.find(id=id)
-    for option in carrier_list:
+    for option in carrier_list.findall('option'):
         options_values.append(option['value'])
     return options_values
 
@@ -23,8 +23,7 @@ def main():
     soup = bs(file.text)
 
     codes = options(soup, 'CarrierList')
-    print_list('Carrieres', codes)
+    print_list('Carriers', codes)
 
     codes = options(soup, 'AirportList')
-    print_list("Airports".codes)
-   
+    print_list("Airports", codes)
