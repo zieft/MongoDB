c4a6d8173014208c4857df40a074062ef896d349
Yulin Zhu <zieft@hotmail.com>
2017-03-14 16:02:41 +0100

    add the codes

--- lesson_2/html_extraction.py
+++ lesson_2/html_extraction.py
@@ -1,1 +1,30 @@
 from bs4 import BeautifulSoup as bs
+import requests
+
+URL = 'https://www.transtats.bts.gov/Data_Elements.aspx?Data=2'
+file = requests.get(URL)
+
+
+def options(soup, id):
+    options_values = []
+    carrier_list = soup.find(id=id)
+    for option in carrier_list:
+        options_values.append(option['value'])
+    return options_values
+
+
+def print_list(label, codes):
+    print "\n{}".format(label)
+    for c in codes:
+        print c
+
+
+def main():
+    soup = bs(file.text)
+
+    codes = options(soup, 'CarrierList')
+    print_list('Carrieres', codes)
+
+    codes = options(soup, 'AirportList')
+    print_list("Airports".codes)
+   
