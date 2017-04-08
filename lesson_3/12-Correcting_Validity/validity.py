660908af5b205086f31ebdd4e27b8b85cca2ba62
Yulin Zhu <zieft@hotmail.com>
2017-04-08 22:04:27 +0200

    add official answer to the question

--- lesson_3/12-Correcting_Validity/validity.py
+++ lesson_3/12-Correcting_Validity/validity.py
@@ -56,7 +56,6 @@ def my_process_file(input_file, output_good, output_bad):
                 else:
                     bad.append(row)
 
-
     # This is just an example on how you can use csv.DictWriter
     # Remember that you have to output 2 files
     with open(output_good, "w") as g:
@@ -69,6 +68,44 @@ def my_process_file(input_file, output_good, output_bad):
         writer = csv.DictWriter(h, delimiter='.', fieldnames=header)
         writer.writeheader()
         for row in bad:
+            writer.writerow(row)
+
+
+def process_file(input_file, output_good, output_bad):
+    # store data into lists for output
+    data_good = []
+    data_bad = []
+    with open(input_file, "r") as f:
+        reader = csv.DictReader(f)
+        header = reader.fieldnames
+        for row in reader:
+            # validate URI value
+            if row['URI'].find("dbpedia.org") < 0:
+                continue
+
+            ps_year = row['productionStartYear'][:4]
+            try: # use try/except to filter valid items
+                ps_year = int(ps_year)
+                row['productionStartYear'] = ps_year
+                if (ps_year >= 1886) and (ps_year <= 2014):
+                    data_good.append(row)
+                else:
+                    data_bad.append(row)
+            except ValueError: # non-numeric strings caught by exception
+                if ps_year == 'NULL':
+                    data_bad.append(row)
+
+    # Write processed data to output files
+    with open(output_good, "w") as good:
+        writer = csv.DictWriter(good, delimiter=",", fieldnames= header)
+        writer.writeheader()
+        for row in data_good:
+            writer.writerow(row)
+
+    with open(output_bad, "w") as bad:
+        writer = csv.DictWriter(bad, delimiter=",", fieldnames= header)
+        writer.writeheader()
+        for row in data_bad:
             writer.writerow(row)
 
 
