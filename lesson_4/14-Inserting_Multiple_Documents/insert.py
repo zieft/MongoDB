d71b8eb1f3162f2344447227d9888da08fc566bd
Yulin Zhu <zieft@hotmail.com>
2017-04-18 19:43:50 +0200

    add my answer

--- lesson_4/14-Inserting_Multiple_Documents/insert.py
+++ lesson_4/14-Inserting_Multiple_Documents/insert.py
@@ -1,15 +1,22 @@
+# -*- coding: utf-8 -*-
+
+import sys
+import os
+
+sys.path.append(os.path.split(os.path.abspath('autos'))[0])  # 将autos.py加入path，这样才能从autos中import
 from autos import process_file
 
 
+
 def insert_autos(infile, db):
-    autos = process_file(infile)
+    data = process_file(infile)
 
     # Your code here. Insert the data in one command
     # autos will be a list of dictionaries, as in the example in the previous video
     # You have to insert data in a collection 'autos'
+    db.autos.insert(data)
 
 
-  
 if __name__ == "__main__":
     
     from pymongo import MongoClient
